17703188bc025287de382941d6b6008e5cf89949
Дарья Горячко <dgoryachko@1442-12250.local>
2026-06-24 17:05:44 +0300

    правки

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),  # ← ДОБАВЬТЕ
+    path('', TemplateView.as_view(template_name='albums/index.html'), name='home'),  # ← ДОБАВЬТЕ
     path('admin/', admin.site.urls),
     path('api/', include('albums.urls')),
 ]
